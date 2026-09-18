085f18c984d75c39a056b053d26ddf928035a26e
JRPrickett <97105476+JRPrickett@users.noreply.github.com>
2026-09-18 17:52:16 +0100

    Product: route public site and PWA app shell

--- app-v2/src/main.tsx
+++ app-v2/src/main.tsx
@@ -2,12 +2,15 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import { AppErrorBoundary } from "./app/AppErrorBoundary";
+import { PublicSite } from "./public/PublicSite";
 import "./styles.css";
+
+const isAppRoute = /^\/app(?:\/|$)/.test(window.location.pathname);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <AppErrorBoundary>
-      <App />
+      {isAppRoute ? <App /> : <PublicSite />}
     </AppErrorBoundary>
   </StrictMode>
 );
